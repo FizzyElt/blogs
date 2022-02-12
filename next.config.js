99b46eb48ac213bcf36ff25fb894ebddb42865df
FizzyElt <fizzyelt8786@gmail.com>
2022-02-12 17:51:39 +0800

    update next path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const R = require('ramda');
 
+const basePath = process.env.NODE_ENV === 'production' ? '/blogs' : '';
+
 module.exports = {
   reactStrictMode: true,
+  basePath,
+  assetPrefix: `${basePath}/`,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       return R.assocPath(['resolve', 'fallback', 'fs'], false)(config);
