2912fbc3a35fa9e5beda5bf2dc2c8a910bb9f612
FizzyElt <fizzyelt8786@gmail.com>
2022-02-13 01:24:35 +0800

    update profile

--- profileData.js
+++ profileData.js
@@ -1,28 +1,26 @@
 import { IoLogoGithub, IoLogoTwitter } from 'react-icons/io5';
 
 export const header = {
-  name: 'Name',
-  avatarLink:
-    'https://cdn.pixabay.com/photo/2022/01/16/15/03/finch-6942278_960_720.jpg',
+  name: 'FizzyElt',
+  avatarLink: 'https://avatars.githubusercontent.com/u/43887006?v=4',
 };
 
 export const footer = {
-  email: 'xxxxx@gmail.com',
+  email: 'fizzyelt8786@gmail.com',
   socialLink: [
     {
-      link: '',
+      link: 'https://github.com/FizzyElt',
       icon: IoLogoGithub,
     },
     {
-      link: '',
+      link: 'https://twitter.com/FizzyElt',
       icon: IoLogoTwitter,
     },
   ],
 };
 
 export const profile = {
-  name: 'Name',
-  avatarLink:
-    'https://cdn.pixabay.com/photo/2022/01/16/15/03/finch-6942278_960_720.jpg',
-  description: 'description',
+  name: 'FizzyElt',
+  avatarLink: 'https://avatars.githubusercontent.com/u/43887006?v=4',
+  description: '這裡是我紀錄想法的地方，包含程式、生活、心靈成長',
 };
