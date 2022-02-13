6b2d7987482c3a48deb112aab84ae82511879367
FizzyElt <fizzyelt8786@gmail.com>
2022-02-13 23:35:42 +0800

    add head info to per pages

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -14,30 +14,43 @@ import {
 
 import PageContainer from '~/containers/PageContainer';
 
+import Head from 'next/head';
+
+import { profile } from '~/profileData';
+
 export default function PostPage(props) {
   return (
-    <PageContainer>
-      <Container maxW='container.lg' py={10}>
-        <VStack align='stretch' mb={10}>
-          <Heading>{props.postData.title}</Heading>
-          <HStack>
-            {props.postData.tags.map((tag, index) => (
-              <Tag key={index} variant='outline' color='yellow.300'>
-                {tag}
-              </Tag>
-            ))}
-          </HStack>
-          <Text color='gray.400' fontWeight='bold'>
-            {props.postData.editTime}
-          </Text>
-        </VStack>
-        <Box
-          className='markdown-body'
-          bgColor='transparent'
-          dangerouslySetInnerHTML={{ __html: props.postData.contentHtml }}
-        />
-      </Container>
-    </PageContainer>
+    <>
+      <Head>
+        <title>{props.postData.title}</title>
+        <meta name='author' content={profile.name} />
+        <meta name='description' content={props.postData.description} />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
+
+      <PageContainer>
+        <Container maxW='container.lg' py={10}>
+          <VStack align='stretch' mb={10}>
+            <Heading>{props.postData.title}</Heading>
+            <HStack>
+              {props.postData.tags.map((tag, index) => (
+                <Tag key={index} variant='outline' color='yellow.300'>
+                  {tag}
+                </Tag>
+              ))}
+            </HStack>
+            <Text color='gray.400' fontWeight='bold'>
+              {props.postData.editTime}
+            </Text>
+          </VStack>
+          <Box
+            className='markdown-body'
+            bgColor='transparent'
+            dangerouslySetInnerHTML={{ __html: props.postData.contentHtml }}
+          />
+        </Container>
+      </PageContainer>
+    </>
   );
 }
 
